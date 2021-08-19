f21bb7fcd1ba1b77424c97d2737359ed9aeadc7d
PsykeDady <psdady@msn.com>
2021-08-19 19:29:26 +0200

    pip auto detect and installer

--- pyrecpro.sh
+++ pyrecpro.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+which pip > /dev/null 2> /dev/null
+
+if (( $?!=0 )); then 
+	echo "you need pip to running this software"
+fi
+
+
+if [[ ! -d  lib/pycparser/pycparser ]]; then 
+	pip install -t lib/pycparser pycparser
+fi
+
 PYTHONPATH="lib:$PYTHONPATH" ./psykeco/pyrecpro/pyrecpro.py "$@"
